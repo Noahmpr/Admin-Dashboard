89ca98c0c71ada66e96aa3f6f50ea624ae0261cf
amir hoseein <amiirhossein.mpr@gmail.com>
2022-05-25 13:37:05 +0430

    set setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css';
 
 const App = () => {
-  return <h1 className='underline text-3xl'> hello tailwind</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content='setting' position='top'>
+              <button
+                type='button'
+                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{
+                  backgroundColor: 'black',
+                  borderRadius: '50px',
+                }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
